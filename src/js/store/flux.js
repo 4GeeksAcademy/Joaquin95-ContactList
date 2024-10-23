6891ec1054b6920db0aeedc12f90342669b48df7
Joaquin Morales <Mintinvestments95@gmail.com>
2024-10-23 00:04:53 +0000

    Need to complete

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,61 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [],
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+			getContacts: () => {
+                fetch('https://playground.4geeks.com/contact/agendas/Joaquin95/contacts')
+                .then((resp) => resp.json())
+                .then((data) => setStore({ contacts: data }))
+                .catch((error) => console.error("Error fetching contacts: ", error));
+            },
+          
+            createUser: (userData) => {
+                fetch("https://playground.4geeks.com/contact/agendas/Joaquin95/contacts", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(userData),
+                })
+                  .then((response) => response.json())
+                  .then((data) => {
+                    console.log("User created successfully:", data);
+                  })
+                  .catch((error) => console.log("Error creating user: ", error));
+              },
+       
+            updateContact: (id, updatedData) => {
+                fetch(`https://playground.4geeks.com/contact/agendas/Joaquin95/contacts/${id}`, {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(updatedData),
+                })
+                    .then(res => res.json())
+                    .then(updatedContact => {
+                        const store = getStore();
+                        setStore({
+                            contacts: store.contacts.map(contact =>
+                                contact.id === id ? updatedContact : contact
+                            ),
+                        });
+                    })
+                    .catch(err => console.error(err));
+            },
+       
+            deleteContact: (id) => {
+                fetch(`https://playground.4geeks.com/contact/agendas/Joaquin95/contacts/${id}`, {
+                    method: 'DELETE',
+                })
+                    .then(() => {
+                        const store = getStore();
+                        setStore({
+                            contacts: store.contacts.filter(contact => contact.id !== id),
+                        });
+                    })
+                    .catch(err => console.error(err));
+            },
+        },
+    };
 };
-
 export default getState;
